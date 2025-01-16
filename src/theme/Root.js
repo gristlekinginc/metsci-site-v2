60fe0b05aa1cb3bdc8e2bae2509c740a04b58fbb
Meteoscientific <nik@meteoscientific.com>
2025-01-15 20:14:32 -0800

    posthog react update web analytics

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { initPostHog } from '../utils/posthog';
-import { useEffect } from 'react';
+import { PostHogProvider } from 'posthog-js/react'
 
 export default function Root({children}) {
-  useEffect(() => {
-    initPostHog();
-  }, []);
+  const options = {
+    api_host: process.env.POSTHOG_HOST,
+  }
 
-  return <>{children}</>;
+  return (
+    <PostHogProvider 
+      apiKey={process.env.POSTHOG_API_KEY}
+      options={options}
+    >
+      {children}
+    </PostHogProvider>
+  );
 }
